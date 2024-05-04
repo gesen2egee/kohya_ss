6a63043e06bc4356dac82786f6bfb21179e86c34
gesen2egee <gesen2gee@gmail.com>
2024-05-04 17:14:46 +0800

    Update train_util.py

--- library/train_util.py
+++ library/train_util.py
@@ -859,7 +859,7 @@ class BaseDataset(torch.utils.data.Dataset):
 
                 flex_tokens = dropout_tags(flex_tokens)
 
-                caption = ", ".join(fixed_tokens + flex_tokens + fixed_suffix_tokens)
+                caption = " ".join(fixed_tokens + flex_tokens + fixed_suffix_tokens)
 
             # process secondary separator
             if subset.secondary_separator:
