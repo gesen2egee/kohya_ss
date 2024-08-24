0805ec6f96adfaca8036e15bf2f8931c03c99b92
gesen2egee <gesen2gee@gmail.com>
2024-08-25 00:13:27 +0800

    Update flux.bat

--- flux.bat
+++ flux.bat
@@ -3,11 +3,14 @@ accelerate launch  --mixed_precision bf16 --num_cpu_threads_per_process 6 flux_t
 :: 先按照GUI的標準方式安裝 然後自己修改FLUX.bat的參數
 :: 用.\venv\Scripts\activate 進入venv再執行FLUX.bat
 
-:: FOR 12G
-:: 16G VRAM 拿掉 --split_mode --network_args "train_blocks=single"
+:: 這個fork增加了 CAME優化器和REX排程 你可以簡單當成是更好的adafactor和更好的cosine
+
+:: 這個設定是FOR 12G
+:: 16G VRAM 拿掉最後面的 --split_mode --network_args "train_blocks=single"
 :: 24G VRAM 再加上 --highvram
 
 :: flux1-dev.safetensors ae.safetensors
 :: 請至 https://huggingface.co/black-forest-labs/FLUX.1-dev/tree/main 下載
+
 :: t5xxl_fp16.safetensors clip_l.safetensors 
 :: 請至 https://huggingface.co/stabilityai/stable-diffusion-3-medium/tree/main/text_encoders 下載
